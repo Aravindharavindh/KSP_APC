6553c70ed950664dea6bd903777809c4caad54e8
IyyappanAvia <iyyappan@avia.co.in>
2022-07-04 10:33:01 +0530

    KSP_APC Commit

---
+++ KSP_APCC/Pages/application-form.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace KSP_APCC.Pages
+{
+    public class application_formModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
---
+++ KSP_APCC/Pages/payment-status.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace KSP_APCC.Pages
+{
+    public class payment_statusModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
